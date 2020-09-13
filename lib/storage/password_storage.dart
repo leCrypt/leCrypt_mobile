517554ef1fef331f9dc6193287cf054710f8b108
Yash Johri <yashjohri1200@gmail.com>
2020-09-13 18:36:23 +0530

    Passwords Storage added

---
+++ lib/storage/password_storage.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
+import 'package:leCrypt_mobile/models/password.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pbkdf2_dart/pbkdf2_dart.dart';
+
+class PasswordStorage {
+  Future<List<Pass>> getPasswords() async {
+    var directory = await getApplicationDocumentsDirectory();
+    var file = File('${directory.path}/password.json');
+    var fileContent = await file.readAsString();
+    var _passes = passwordsFromJson(fileContent);
+    return _passes.passes;
+  }
+
+  Future<void> DeletPassword(int index) async {
+    var passes = await getPasswords();
+    passes.removeAt(index);
+    await writePassword(passes);
+  }
+
+  Future<void> writePassword(List<Pass> passes) async {
+    var directory = await getApplicationDocumentsDirectory();
+    var file = File('${directory.path}/password.json');
+    var exist = await file.exists();
+    if (!exist) {
+      await file.create().then((value) {
+        value.writeAsString(
+          passwordsToJson(Passwords(passes: passes)),
+        );
+      });
+    } else {
+      await file.writeAsString(
+        passwordsToJson(Passwords(passes: passes)),
+      );
+    }
+  }
+
+  Future<void> addNote(String website, String username, String password) async {
+    var notes = await getPasswords();
+    notes.add(
+      Pass(
+        website: convertStringToHash(website),
+        username: convertStringToHash(username),
+        password: convertStringToHash(password),
+      ),
+    );
+    await writePassword(notes);
+  }
+
+  String convertStringToHash(String value) {
+    var hash = '';
+    var gen = PBKDF2(hash: sha1);
+    var key = gen.generateKey(value, 'salt', 1000, 16);
+    for (var a in key) {
+      hash += a.toRadixString(16);
+    }
+    return hash;
+  }
+}
